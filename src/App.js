3ec641ed45caaab8209e60d00cec0b50a83dec34
賬號已註銷 <wbyanyue@centaline.com.cn>
2020-07-22 18:03:14 +0800

    进度条提交

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.scss'
-import { Layout, Menu } from 'antd';
+import { Layout, Menu,Dropdown } from 'antd';
 import {createFromIconfontCN,MailOutlined, UserOutlined, LaptopOutlined, NotificationOutlined,MenuUnfoldOutlined,MenuFoldOutlined} from '@ant-design/icons';
-import SubMenu from 'antd/lib/menu/SubMenu';
+import {BrowserRouter as Router,Route,Link, Redirect} from "react-router-dom";
+import menu from './assets/js/menu'
+import HomePage from './views/homepage'
+
 const { Header, Content, Sider } = Layout;
 const Iconfont=createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_8d5l8fzk5b87iudi.js',
+  scriptUrl: '//at.alicdn.com/t/font_1953884_rg89im7c0ij.js',
 });
 class App extends Component {
   constructor(props){
@@ -32,14 +35,32 @@ class App extends Component {
               <Menu.Item key="2">nav 2</Menu.Item>
               <Menu.Item key="3">nav 3</Menu.Item>
             </Menu>
+            <div className="right-my">
+              <div className="download">
+                <Iconfont type="icon-phone" className="icon" style={{transform: 'scale(0.6)'}}/>
+              </div>
+              <div className="text">
+                <span>下载APP</span>
+              </div>
+              <div className="info">
+                <Iconfont type="icon-xiaoxi1" className="icon"  style={{transform: 'scale(0.7)'}}/>
+              </div>
+              <div className="avatar">
+                <img src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>
+              </div>
+              <div className="userName">
+                <span>我是总行</span>
+              </div>
+              <div className="drop">
+                <Dropdown overlay={menu} placement="bottomRight" arrow overlayStyle={{borderRadius:'50%'}}>
+                  <Iconfont type="icon-zhankaigengduo" className="icon"  style={{transform: 'scale(0.6)'}}/>
+                </Dropdown>
+              </div>
+            </div>
           </Header>
-          
           <Layout>
-            
             <Sider width={140} collapsible trigger={null} collapsed={this.state.collapsed}>
-
               <Menu mode="inline" defaultSelectedKeys={['9']}  style={{ height: '100%', borderRight: 0 }}>
-
                   <Menu.Item key="9"  icon={<UserOutlined/>}>option9</Menu.Item>
                   <Menu.Item key="10" icon={<LaptopOutlined/>}>option10</Menu.Item>
                   <Menu.Item key="11" icon={<NotificationOutlined/>}>option11</Menu.Item>
@@ -59,8 +80,13 @@ class App extends Component {
                   minHeight: 280,
                 }}
               >
-                Content<br/>
-                qwqewqre
+                <Router>
+                  <Route exact path="/" render={()=>(
+                    <Redirect to="/homepage"/>
+                  )}></Route>
+                  <Route path="/homepage" exact component={HomePage}></Route>
+                </Router>
+
               </Content>
             </Layout>
           </Layout>
---
+++ src/assets/js/menu.js
@@ -0,0 +1,32 @@
+import {Menu} from 'antd'
+import React from 'react';
+const menu=(
+<Menu>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+        切换账户
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+        个人中心
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+        帮助中心
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+        修改密码
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+        登出
+      </a>
+    </Menu.Item>
+</Menu>
+)
+export default menu
